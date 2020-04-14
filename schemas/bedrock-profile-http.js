66d7256d6b31d49a2943286cc5b92d276701e61a
Andrew L Jones <ajones@digitalbazaar.com>
2020-04-14 17:57:56 -0400

    Start validators for all endpoints.

--- schemas/bedrock-profile-http.js
+++ schemas/bedrock-profile-http.js
@@ -5,8 +5,141 @@
 
 const {schemas} = require('bedrock-validation');
 
-// createProfileAgent
-// createProfileAgents
-// byAccount
-// delegateCapability
-// zcaps
+const account = {
+  title: 'Account',
+  type: 'string'
+};
+
+const profile = {
+  title: 'Profile',
+  type: 'string'
+};
+
+// this should match query objects with an account in them
+const accountQuery = {
+  title: 'Accout Query',
+  type: 'object',
+  required: ['account'],
+  // FIXME set this to false once we have
+  // decided on all the properties.
+  additionalProperties: true,
+  properties: {account}
+};
+
+const profileAgent = {
+  title: 'Profile Agent',
+  type: 'object',
+  required: ['account', 'profile', 'token'],
+  // FIXME set this to false once we have
+  // decided on all the properties.
+  additionalProperties: true,
+  properties: {
+    account,
+    profile,
+    token: {
+      title: 'Token',
+      type: 'string'
+    }
+  }
+};
+
+const profileAgents = {
+  title: 'Profile Agents',
+  type: 'object',
+  required: ['account', 'profile'],
+  // FIXME set this to false once we have
+  // decided on all the properties.
+  additionalProperties: true,
+  properties: {
+    account,
+    profile
+  }
+};
+
+const delegateCapability = {
+  title: 'Delegate Capability',
+  type: 'object',
+  required: ['invoker', 'account'],
+  // FIXME set this to false if we
+  // have decided on all the properties
+  additionalProperties: true,
+  properties: {
+    account,
+    invoker: {
+      anyOf: [{
+        type: 'string'
+      }, {
+        type: 'array',
+        minItems: 1,
+        items: [{type: 'string'}]
+      }]
+    }
+  }
+};
+
+const zcap = {
+  title: 'zcap',
+  type: 'object',
+  // FIXME set this to false once we have
+  // decided on all the properties.
+  additionalProperties: true,
+  properties: {
+    id: {
+      title: 'id',
+      type: 'string'
+    },
+    controller: {
+      title: 'controller',
+      type: 'string'
+    },
+    allowedAction: {
+      anyOf: [{
+        type: 'string'
+      }, {
+        type: 'array',
+        minItems: 1,
+        items: [{type: 'string'}]
+      }]
+    },
+    invoker: {
+      anyOf: [{
+        type: 'string'
+      }, {
+        type: 'array',
+        minItems: 1,
+        items: [{type: 'string'}]
+      }]
+    },
+    invocationTarget: {
+      title: 'Invocation Target',
+      anyOf: [{
+        type: 'string'
+      }, {
+        type: 'object',
+        properties: {
+          id: {
+            title: 'Invocation Target Id',
+            type: 'string'
+          },
+          type: {
+            title: 'Invocation Target Type',
+            type: 'string'
+          }
+        }
+      }]
+    }
+  }
+};
+
+const zcaps = {
+  title: 'zcaps',
+  type: 'array',
+  minItems: 1,
+  items: [zcap]
+};
+
+module.exports.profileAgent = () => profileAgent;
+module.exports.profileAgents = () => profileAgents;
+module.exports.account = () => accountQuery;
+module.exports.delegateCapability = () => delegateCapability;
+module.exports.zcaps = () => zcaps;
