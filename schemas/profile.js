7d797617a411d1133fc43e546f88420641eeaf44
andrewjones <ajones@digitalbazaar.com>
2020-04-14 15:22:46 -0400

    Layout validators for all routes.

---
+++ schemas/profile.js
@@ -0,0 +1,14 @@
+/*!
+ * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+const {schemas} = require('bedrock-validation');
+
+
+// createProfile
+// createProfileAgent
+// createProfileAgents
+// byAccount
+// delegateCapability
+// zcaps
