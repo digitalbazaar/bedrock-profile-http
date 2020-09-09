a8d36d4960c0858713f80bfc47c0aaa8163a6fc9
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2020-09-09 12:30:03 -0400

    Update schema for `expires` property.

--- schemas/bedrock-profile-http.js
+++ schemas/bedrock-profile-http.js
@@ -161,8 +161,17 @@ const zcap = {
       type: 'string'
     },
     expires: {
-      title: 'Expiry Date',
-      type: 'string'
+      title: 'W3C Date/Time',
+      description: 'A W3C-formatted date and time combination.',
+      type: 'string',
+      pattern: '^[1-9][0-9]{3}-(0[1-9]|1[0-2])-([0-2][0-9]|3[0-1])' +
+        'T([0-1][0-9]|2[0-3]):([0-5][0-9]):(([0-5][0-9])|60)(\\.[0-9]+)?' +
+        '(Z|((\\+|-)([0-1][0-9]|2[0-3]):([0-5][0-9])))?$',
+      errors: {
+        invalid: 'The date/time must be of the W3C date/time format ' +
+          '"YYYY-MM-DD( |T)HH:MM:SS.s(Z|(+|-)TZOFFSET)".',
+        missing: 'Please enter a date/time.'
+      }
     }
   }
 };
