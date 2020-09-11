3da1420874a5bdf2b3ff5d7da8606b954d59fb39
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2020-09-10 21:23:08 -0400

    Update `eslint-config-digitalbazaar` and `eslint` to latest.
    - Just for testing purpose update `bedrock-profile` to the branch
      which has changes.
    closes #16

--- test/mocha/10-api.js
+++ test/mocha/10-api.js
@@ -547,18 +547,22 @@ describe('bedrock-profile-http', () => {
     });
     it('should not throw error if zcap `expires` pattern does not ' +
       'include millisecond', async () => {
-      const zcaps = mockData.zcaps2;
       const {account: {id: account}} = accounts['alpha@example.com'];
       const {data: {id: profile}} = await api.post('/profiles', {account});
       const {data} = await api.get(`/profile-agents/?account=${account}` +
         `&profile=${profile}`);
+      const zcapsCopy = {...zcaps};
+      for(const prop in zcapsCopy) {
+        // change expires to not have millisecond
+        zcapsCopy[prop].expires = '2050-09-10T15:56:22';
+      }
       const {id: profileAgentId} = data[0].profileAgent;
       let result;
       let result0;
       let error;
       try {
         result = await api.post(`/profile-agents/${profileAgentId}` +
-          `/capability-set?account=${account}`, {zcaps});
+          `/capability-set?account=${account}`, {zcaps: zcapsCopy});
         result0 = await api.get(`/profile-agents/${profileAgentId}` +
           `?account=${account}`);
       } catch(e) {
@@ -568,7 +572,7 @@ describe('bedrock-profile-http', () => {
       should.exist(result);
       result.status.should.equal(204);
       result.ok.should.equal(true);
-      should.exist(result0.data.profileAgent.zcaps.zero.expires);
+      should.exist(result0.data.profileAgent.zcaps);
     });
     it('throws error when there is no zcaps', async () => {
       const {account: {id: account}} = accounts['alpha@example.com'];
