374a0a4a8797d05be0d05cd2a0ff986cf6e985ee
Dave Longley <dlongley@digitalbazaar.com>
2021-08-16 12:44:54 -0400

    Create new meter for each profile; pass tests.

--- test/test.js
+++ test/test.js
@@ -9,6 +9,9 @@ require('bedrock-https-agent');
 require('bedrock-jsonld-document-loader');
 require('bedrock-kms');
 require('bedrock-kms-http');
+require('bedrock-meter');
+require('bedrock-meter-usage-reporter');
+const {handlers} = require('bedrock-meter-http');
 require('bedrock-passport');
 require('bedrock-permission');
 require('bedrock-profile');
@@ -16,4 +19,20 @@ require('bedrock-profile-http');
 require('bedrock-server');
 require('bedrock-ssm-mongodb');
 require('bedrock-test');
+
+bedrock.events.on('bedrock.init', async () => {
+  /* Handlers need to be added before `bedrock.start` is called. These are
+  no-op handlers to enable meter usage without restriction */
+  handlers.setCreateHandler({
+    handler({meter} = {}) {
+      // use configured meter usage reporter as service ID for tests
+      meter.serviceId = bedrock.config['meter-usage-reporter'].client.id;
+      return {meter};
+    }
+  });
+  handlers.setUpdateHandler({handler: ({meter} = {}) => ({meter})});
+  handlers.setRemoveHandler({handler: ({meter} = {}) => ({meter})});
+  handlers.setUseHandler({handler: ({meter} = {}) => ({meter})});
+});
+
 bedrock.start();
