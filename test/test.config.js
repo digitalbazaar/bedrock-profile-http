b9988ec73e13c8a5eeba9c50cd3f017f025dc107
Dave Longley <dlongley@digitalbazaar.com>
2025-09-18 00:06:26 -0400

    Update copyright header dates.
    Co-authored-by: David I. Lehn <dlehn@digitalbazaar.com>

--- test/test.config.js
+++ test/test.config.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright (c) 2020-2024 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2020-2025 Digital Bazaar, Inc. All rights reserved.
  */
 import {config} from '@bedrock/core';
 import {fileURLToPath} from 'node:url';
